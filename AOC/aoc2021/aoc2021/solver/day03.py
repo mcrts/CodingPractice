10b6c88d19c6eb347980bcbf00ec06c070571bb7
Martin Courtois <martin.courtois@protonmail.com>
2022-11-10 23:18:17 +0100

    [FIX] day 03

--- AOC/aoc2021/aoc2021/solver/day03.py
+++ AOC/aoc2021/aoc2021/solver/day03.py
@@ -1,94 +1,109 @@
 from collections import Counter
 import functools
 import itertools as it
-from typing import Callable, Iterable, Sequence
+from typing import Callable, Iterable, Sequence, Self
+from pydantic import BaseModel
 
 from aoc2021.utils import T, input_path, Solver
 
 INPATH = input_path(3)
 
 
-def most_common_bits(instream: Iterable[Sequence[T]]) -> Sequence[T]:
-    stream = map(lambda s: tuple(map(lambda x: Counter([x]), s)), instream)
-    res = functools.reduce(
-        lambda x, y: tuple(i[0] + i[1] for i in zip(x, y)),
-        stream,
-    )
-    res = tuple(map(lambda c: c.most_common(1)[0][0], res))
-    return res
+def partition(pred, iterable):
+    "Use a predicate to partition entries into false entries and true entries"
+    t1, t2 = it.tee(iterable)
+    return it.filterfalse(pred, t1), filter(pred, t2)
 
 
-def oxygen_critera(instream: Iterable[str]) -> str:
-    c = Counter(instream)
-    if c["0"] == c["1"]:
-        return "1"
-    else:
+class Reading(BaseModel):
+    bits: Sequence[str]
+
+    @classmethod
+    def fromString(cls, s: str) -> Self:
+        bits = list(s)
+        return cls(bits=bits)
+
+    def __len__(self) -> int:
+        return len(self.bits)
+
+    def read_at(self, i: int) -> str:
+        return self.bits[i]
+
+
+class Device(BaseModel):
+    readings: Sequence[Reading]
+
+    @property
+    def bit_length(self) -> int:
+        return len(self.readings[0])
+
+    def most_common_bit(self, idx: int) -> str:
+        bits = map(lambda r: Reading.read_at(r, idx), self.readings)
+        c = Counter(bits)
         return c.most_common(1)[0][0]
 
-
-def co2_criteria(instream: Iterable[str]) -> str:
-    c = Counter(instream)
-    if c["0"] == c["1"]:
-        return "0"
-    else:
+    def least_common_bit(self, idx: int) -> str:
+        bits = map(lambda r: Reading.read_at(r, idx), self.readings)
+        c = Counter(bits)
         return c.most_common()[-1][0]
 
+    def gamma_rate(self) -> int:
+        bits = [self.most_common_bit(i) for i in range(0, self.bit_length)]
+        rate = int("".join(bits), base=2)
+        return rate
 
-def compute_bit_criteria(
-    instream: Iterable[Sequence[str]], at: int, criteria: Callable
-) -> str:
-    stream = map(lambda t: t[at], instream)
-    return criteria(stream)
+    def epsilon_rate(self) -> int:
+        bits = [self.least_common_bit(i) for i in range(0, self.bit_length)]
+        rate = int("".join(bits), base=2)
+        return rate
 
+    def o2_rating(self, i: int = 0) -> int:
+        if len(self.readings) == 1:
+            return int("".join(self.readings[0].bits), base=2)
 
-def oxygen_generator_rating(instream: Iterable[Sequence[str]]) -> int:
-    stream, _instream = it.tee(instream, 2)
-    bits = ""
-    for i in range(12):
-        stream, _instream = it.tee(_instream, 2)
-        stream = filter(lambda s: "".join(s).startswith(bits), stream)
-        bits += compute_bit_criteria(stream, i, oxygen_critera)
+        readings0, readings1 = partition(lambda x: x.read_at(i) == "1", self.readings)
+        readings0 = list(readings0)
+        readings1 = list(readings1)
+        if len(readings0) > len(readings1):
+            return Device(readings=readings0).o2_rating(i + 1)
+        else:
+            return Device(readings=readings1).o2_rating(i + 1)
 
-    rating = int(bits, 2)
-    return rating
+    def co2_rating(self, i: int = 0) -> int:
+        if len(self.readings) == 1:
+            return int("".join(self.readings[0].bits), base=2)
 
-
-def co2_scrubber_rating(instream: Iterable[Sequence[str]]) -> int:
-    stream, _instream = it.tee(instream, 2)
-    bits = ""
-    for i in range(12):
-        stream, _instream = it.tee(_instream, 2)
-        stream = filter(lambda s: "".join(s).startswith(bits), stream)
-        bits += compute_bit_criteria(stream, i, co2_criteria)
-    rating = int(bits, base=2)
-    return rating
-
-
-def solve_part1(instream: Iterable[Sequence[str]]) -> int:
-    base = int("0b111111111111", base=0)
-    gamma_bits = "".join(most_common_bits(instream))
-    gamma_rate = int(gamma_bits, 2)
-    epsilon_rate = base ^ gamma_rate
-    return gamma_rate * epsilon_rate
+        readings0, readings1 = partition(lambda x: x.read_at(i) == "1", self.readings)
+        readings0 = list(readings0)
+        readings1 = list(readings1)
+        if len(readings0) <= len(readings1):
+            return Device(readings=readings0).co2_rating(i + 1)
+        else:
+            return Device(readings=readings1).co2_rating(i + 1)
 
 
-def solve_part2(instream: Iterable[Sequence[str]]) -> int:
-    s1, s2 = it.tee(instream, 2)
-    r1 = oxygen_generator_rating(s1)
-    r2 = co2_scrubber_rating(s2)
-    return r1 * r2
+def solve_part1(device: Device) -> int:
+    return device.gamma_rate() * device.epsilon_rate()
 
 
-def parse_input(instream: Iterable[str]) -> Iterable[Sequence[str]]:
-    return map(lambda s: tuple(s), instream)
+def solve_part2(device: Device) -> int:
+    o2_rating = device.o2_rating()
+    co2_rating = device.co2_rating()
+    return o2_rating * co2_rating
+
+
+def parse_input(readings: Iterable[Reading]) -> Device:
+    return Device(readings=list(readings))
 
 
 solver01 = Solver(
+    lineparser=Reading.fromString,  # type: ignore
     parser=parse_input,  # type: ignore
     solver=solve_part1,  # type: ignore
 )
 
 solver02 = Solver(
+    lineparser=Reading.fromString,  # type: ignore
     parser=parse_input,  # type: ignore
     solver=solve_part2,  # type: ignore
 )
