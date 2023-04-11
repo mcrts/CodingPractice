9ed4619a0997e0ec4b648d27679145278f9f4dc3
Martin Courtois <martin.courtois@protonmail.com>
2023-04-11 16:30:41 +0200

    fix: baseline code-vs-zombies py

--- CodinGame/Optimization/code-vs-zombies.py
+++ CodinGame/Optimization/code-vs-zombies.py
@@ -1,37 +1,64 @@
 import sys
 import math
+import numpy as np
 
-# Save humans, destroy zombies!
+def debug(*args):
+    print(*args, file=sys.stderr, flush=True)
 
-def distance(a, b):
-    xa, ya = a
-    xb, yb = b
-    return math.sqrt((xb - xa)**2 + (yb - ya)**2)
+ASH_SPD = 1000
+Z_SPD = 400
+ASH_RAD = 2000
+Z_RAD = 400
 
+
+def is_saveable(player, human, zombies):
+    t_ash = math.ceil(
+        (np.linalg.norm(player - human) - ASH_RAD) / ASH_SPD
+    )
+    f_t_zombie = lambda z: math.ceil(
+        (np.linalg.norm(z - human) - Z_RAD) / Z_SPD
+    )
+    f_filter = lambda z: t_ash - 1 > f_t_zombie(z)
+    zombies = filter(f_filter, zombies.values())
+    return len(list(zombies)) == 0
+
+def attack(player, humans, zombies):
+    t = next(iter(zombies.values()))
+    return t
+
+def defend_first(player, humans, zombies):
+    t = next(iter(humans.values()))
+    return t
+
+def defend_closest(player, humans, zombies):
+    f_dist = lambda x: np.linalg.norm(x - player)
+    humans_pos = sorted(humans.values(), key=f_dist)
+    return humans_pos[0]
+
+def defend_closest_saveable(player, humans, zombies):
+    humans = humans.values()
+    humans = filter(lambda h: is_saveable(player, h, zombies), humans)
+    f_dist = lambda x: np.linalg.norm(x - player)
+    humans = sorted(humans, key=f_dist)
+    return humans[0]
 
 # game loop
 while True:
     x, y = [int(i) for i in input().split()]
+    player = np.array((x, y), dtype=int)
     human_count = int(input())
     humans = dict()
-    for i in range(human_count):
-        human_id, human_x, human_y = [int(j) for j in input().split()]
-        humans[human_id] = (human_x, human_y)
+    for _ in range(human_count):
+        i, x, y = [int(j) for j in input().split()]
+        humans[i] = np.array((x, y), dtype=int)
+    
     zombie_count = int(input())
     zombies = dict()
     for i in range(zombie_count):
-        zombie_id, zombie_x, zombie_y, zombie_xnext, zombie_ynext = [int(j) for j in input().split()]
-        zombies[zombie_id] = (zombie_x, zombie_y)
+        i, x, y, _, _ = [int(j) for j in input().split()]
+        zombies[i] = np.array((x, y), dtype=int)
 
-    human_center = (sum(map(lambda x: x[0], humans.values()))/human_count, sum(map(lambda x: x[1], humans.values()))/human_count)
-    distances = list(map(lambda x: distance(human_center, x), humans.values()))
-    if list(filter(lambda x: x<=2000, distances)):
-        target = human_center
-    else:
-        target = list(humans.values())[0]
-    print(distances, file=sys.stderr, flush=True)
-    print('{} {}'.format(round(target[0]), round(target[1])))
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    target = defend_closest_saveable(player, humans, zombies)
 
-    # Your destination coordinates
+    print(f"{target[0]} {target[1]}")
+
