b7ce4d7ff2fd3b432d421c657811a101a5d769ef
Martin Courtois <martin.courtois@protonmail.com>
2024-12-06 14:44:31 +0100

    add: day02

---
+++ AOC/aoc2024/day02.py
@@ -0,0 +1,66 @@
+import itertools as I
+import math as M
+import sys
+from collections import Counter
+from typing import Iterator
+
+
+def is_safe(report):
+    delta = tuple(I.starmap(int.__sub__, zip(report[0:-1], report[1:])))
+    c = Counter(delta)
+    is_all_positive = all(map(lambda v: v > 0, c.keys()))
+    is_all_negative = all(map(lambda v: v < 0, c.keys()))
+    is_within_range = all(tuple(map(lambda v: 0 < abs(v) < 4, c.keys())))
+
+    return is_within_range and (is_all_positive or is_all_negative)
+
+
+def is_safe_dampened(report):
+    for i in range(len(report)):
+        r = list(report)
+        r.pop(i)
+        if is_safe(r):
+            return True
+
+    return False
+
+
+def part01(pipe: Iterator[str]):
+    reports = []
+    for l in pipe:
+        r = tuple(map(int, l.strip().split(" ")))
+        reports.append(r)
+
+    count = 0
+    for r in reports:
+        if is_safe(r):
+            count += 1
+    return count
+
+
+def part02(pipe: Iterator[str]):
+    reports = []
+    for l in pipe:
+        r = tuple(map(int, l.strip().split(" ")))
+        reports.append(r)
+
+    count = 0
+    for r in reports:
+        flag = is_safe(r)
+        if not flag:
+            flag = is_safe_dampened(r)
+
+        if flag:
+            count += 1
+
+    return count
+
+
+def main():
+    p1, p2 = I.tee(sys.stdin, 2)
+    print(f"Part01 | {part01(p1)}")
+    print(f"Part02 | {part02(p2)}")
+
+
+if __name__ == "__main__":
+    main()
--- AOC/aoc2024/template.py
+++ AOC/aoc2024/template.py
@@ -1,20 +1,19 @@
-import itertools as I
+import os
+import shutil
 import sys
-from typing import Iterator
+from pathlib import Path
 
-
-def part01(pipe: Iterator[str]):
-    return 0
-
-
-def part02(pipe: Iterator[str]):
-    return 0
+DIR = Path(os.path.dirname(os.path.realpath(__file__)))
+TEMPLATE = DIR / "template_source.py"
+FILES = DIR / "files"
+TESTFILES = DIR / "files/test"
 
 
 def main():
-    p1, p2 = I.tee(sys.stdin, 2)
-    print(f"Part01 | {part01(p1)}")
-    print(f"Part02 | {part02(p2)}")
+    day = int(sys.argv[1])
+    shutil.copyfile(TEMPLATE, DIR / f"day{day:02}.py")
+    open(FILES / f"day{day:02}.txt", "a").close()
+    open(TESTFILES / f"day{day:02}.txt", "a").close()
 
 
 if __name__ == "__main__":
