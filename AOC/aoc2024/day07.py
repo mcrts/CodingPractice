a5d677289e5ea41b34b87e43daa439dc2add728d
Martin Courtois <martin.courtois@protonmail.com>
2024-12-16 16:09:17 +0100

    wip day07

---
+++ AOC/aoc2024/day07.py
@@ -0,0 +1,22 @@
+import itertools as I
+import sys
+from pprint import pprint
+from typing import Iterator
+
+
+def part01(pipe: Iterator[str]):
+    return 0
+
+
+def part02(pipe: Iterator[str]):
+    return 0
+
+
+def main():
+    p1, p2 = I.tee(sys.stdin, 2)
+    print(f"Part01 | {part01(p1)}")
+    print(f"Part02 | {part02(p2)}")
+
+
+if __name__ == "__main__":
+    main()
