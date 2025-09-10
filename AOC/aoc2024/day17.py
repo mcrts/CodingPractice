79e2cd19c7734f8cd45e4a230d8e20b2378bbe31
Martin COURTOIS <martin.courtois@protonmail.com>
2025-09-10 15:26:44 +0200

    wip: day17

--- AOC/aoc2024/day17.py
+++ AOC/aoc2024/day17.py
@@ -1,9 +1,22 @@
+import functools as F
 import itertools as I
 import re
 import sys
 from dataclasses import dataclass, field
+from itertools import tee
 from pprint import pprint
-from typing import Callable, ClassVar, Iterable, Iterator, List, Mapping, Tuple
+from types import GeneratorType
+from typing import (
+    Callable,
+    ClassVar,
+    Iterable,
+    Iterator,
+    List,
+    Mapping,
+    NamedTuple,
+    Self,
+    Tuple,
+)
 
 RE_REG = r"Register ([ABC]): (\d+)"
 RE_PROG = r"Program: (([0-8],{0,1})+)"
@@ -73,16 +86,7 @@ class Computer:
         self.pointer += 2
 
     def run(self, program: Tuple[int, ...]) -> Tuple[int, ...]:
-        opcodes = [
-            self.adv,
-            self.bxl,
-            self.bst,
-            self.jnz,
-            self.bxc,
-            self.out,
-            self.bdv,
-            self.cdv,
-        ]
+        opcodes = [self.adv, self.bxl, self.bst, self.jnz, self.bxc, self.out, self.bdv, self.cdv]
         while self.pointer < len(program) - 1:
             instr, operand = program[self.pointer : self.pointer + 2]
             opcodes[instr](operand)
@@ -109,14 +113,162 @@ def part01(pipe: Iterator[str]):
     return ",".join(map(str, output))
 
 
+Output = Tuple[int, ...]
+
+
+class ComputerState(NamedTuple):
+    program: Tuple[int, ...]
+    ra: int
+    rb: int
+    rc: int
+    pointer: int
+
+    @classmethod
+    def make(cls, program: Tuple[int, ...], ra: int, rb: int, rc: int, pointer: int) -> Self:
+        return cls(program, ra, rb, rc, pointer)
+
+    def combo(self, operand: int) -> int:
+        match operand:
+            case 4:
+                return self.ra
+            case 5:
+                return self.rb
+            case 6:
+                return self.rc
+            case 7:
+                raise ValueError("Unexpected operand 7")
+            case x:
+                return x
+
+    def adv(self, operand: int) -> Tuple[Self, Output]:
+        """opcode 0"""
+        ra = self.ra // 2 ** self.combo(operand)
+        pointer = self.pointer + 2
+        return (
+            self.make(program=self.program, ra=ra, rb=self.rb, rc=self.rc, pointer=pointer),
+            tuple(),
+        )
+
+    def bxl(self, operand: int) -> Tuple[Self, Output]:
+        """opcode 1"""
+        rb = self.rb ^ operand
+        pointer = self.pointer + 2
+        return (
+            self.make(program=self.program, ra=self.ra, rb=rb, rc=self.rc, pointer=pointer),
+            tuple(),
+        )
+
+    def bst(self, operand: int) -> Tuple[Self, Output]:
+        """opcode 2"""
+        rb = self.combo(operand) % 8
+        pointer = self.pointer + 2
+        return (
+            self.make(program=self.program, ra=self.ra, rb=rb, rc=self.rc, pointer=pointer),
+            tuple(),
+        )
+
+    def jnz(self, operand: int) -> Tuple[Self, Output]:
+        """opcode 3"""
+        if self.ra == 0:
+            pointer = self.pointer + 2
+        else:
+            pointer = operand
+        return (
+            self.make(program=self.program, ra=self.ra, rb=self.rb, rc=self.rc, pointer=pointer),
+            tuple(),
+        )
+
+    def bxc(self, operand: int) -> Tuple[Self, Output]:
+        """opcode 4"""
+        rb = self.rb ^ self.rc
+        pointer = self.pointer + 2
+        return (
+            self.make(program=self.program, ra=self.ra, rb=rb, rc=self.rc, pointer=pointer),
+            tuple(),
+        )
+
+    def out(self, operand: int) -> Tuple[Self, Output]:
+        """opcode 5"""
+        output = (self.combo(operand) % 8,)
+        pointer = self.pointer + 2
+        return (
+            self.make(program=self.program, ra=self.ra, rb=self.rb, rc=self.rc, pointer=pointer),
+            output,
+        )
+
+    def bdv(self, operand: int) -> Tuple[Self, Output]:
+        """opcode 6"""
+        rb = self.ra // 2 ** self.combo(operand)
+        pointer = self.pointer + 2
+        return (
+            self.make(program=self.program, ra=self.ra, rb=rb, rc=self.rc, pointer=pointer),
+            tuple(),
+        )
+
+    def cdv(self, operand: int) -> Tuple[Self, Output]:
+        """opcode 7"""
+        rc = self.ra // 2 ** self.combo(operand)
+        pointer = self.pointer + 2
+        return (
+            self.make(program=self.program, ra=self.ra, rb=self.rb, rc=rc, pointer=pointer),
+            tuple(),
+        )
+
+    def run_once(self) -> Tuple[bool, Self, Output]:
+        opcodes = [self.adv, self.bxl, self.bst, self.jnz, self.bxc, self.out, self.bdv, self.cdv]
+        if self.pointer < len(self.program) - 1:
+            instr, operand = self.program[self.pointer : self.pointer + 2]
+            cs, output = opcodes[instr](operand)
+            return False, cs, output
+        else:
+            return True, self, tuple()
+
+
+@F.lru_cache()
+def run_once(cs: ComputerState) -> Tuple[bool, ComputerState, Output]:
+    stop = False
+    out = tuple()
+    while not stop:
+        stop, cs, out = cs.run_once()
+        if out:
+            return stop, cs, out
+    return stop, cs, out
+
+
+def streaming_run(cs: ComputerState) -> Iterator[int]:
+    stop = False
+    while not stop:
+        stop, cs, out = run_once(cs)
+        if out:
+            yield out[0]
+
+
+def stream_compare(s0: Iterator[int], s1: Iterator[int]) -> bool:
+    for x, y in I.zip_longest(s0, s1, fillvalue=None):
+        if x != y:
+            return False
+
+    return True
+
+
 def part02(pipe: Iterator[str]):
     """SOLVE with DP, need a static computer state"""
     computer, program = parse_input(pipe)
-    a = 0
-    while True:
-        c = Computer(ra=a, rb=computer.rb, rc=computer.rc)
-        output = c.run(program)
-        if output == program:
+    rb = int(computer.rb)
+    rc = int(computer.rc)
+
+    for a in I.count(0, 1):
+        if a % 1000000 == 0:
+            print(a)
+        cs = ComputerState(program, a, rb, rc, 0)
+        if stream_compare(program, streaming_run(cs)):
+            print(program, tuple(streaming_run(cs)))
             return a
-        a += 1
+
+    """
+    for a in I.count(0, 1):
+        cs = ComputerState(program, a, rb, rc, 0)
+        if run(cs) == program:
+            return a
+    """
     return -1
